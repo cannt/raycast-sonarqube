8ddebe6f65ba5e374269d0c25b865510c1eceeb8
cannt <j.a.t.j.9823@gmail.com>
2025-06-18 11:11:35 +0200

    chore: update eslint config with TypeScript support and remove prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-  extends: ["@raycast/eslint-config", "plugin:prettier/recommended"]
+  extends: ["@raycast/eslint-config", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ["@typescript-eslint"]
 };
